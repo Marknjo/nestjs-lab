865520813f5d37f0de6de9c214730a45c6196042
Marknjo <marknjo7@gmail.com>
2022-09-29 10:42:47 +0300

    perf(coffeeluv): add validations to the create coffee dto

--- coffeeluv/src/coffees/dto/create-coffee.dto.ts
+++ coffeeluv/src/coffees/dto/create-coffee.dto.ts
@@ -1,7 +1,14 @@
+import { IsString, MaxLength } from 'class-validator';
+
 export class CreateCoffeeDto {
+  @MaxLength(50)
+  @IsString()
   readonly name: string;
 
+  @MaxLength(40)
+  @IsString()
   readonly brand: string;
 
+  @IsString({ each: true })
   readonly flavors: Array<string>;
 }
