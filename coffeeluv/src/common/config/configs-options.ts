68c9256981ab3fbcb575eceae63e307a56d6c3fe
Marknjo <marknjo7@gmail.com>
2022-10-08 10:40:43 +0300

    feat(coffeeluv): add joi validation schema to database connection values

--- coffeeluv/src/common/config/configs-options.ts
+++ coffeeluv/src/common/config/configs-options.ts
@@ -1,8 +1,21 @@
-/// Database Configuration Options
-const configDefaultOptions = {};
+import { ConfigModuleOptions } from '@nestjs/config';
+import * as Joi from 'joi';
 
-export const configDevOptions = { ...configDefaultOptions };
-export const configProdOptions = {
+/// Database Configuration Options
+const configDefaultOptions: ConfigModuleOptions = {
+  validationSchema: Joi.object({
+    DB_HOST: Joi.required(),
+    DB_PORT: Joi.number().default(5432),
+    DB_NAME: Joi.string().default('postgres'),
+    DB_USER: Joi.required(),
+    DB_PASS: Joi.required(),
+  }),
+};
+
+export const configDevOptions: ConfigModuleOptions = {
+  ...configDefaultOptions,
+};
+export const configProdOptions: ConfigModuleOptions = {
   ignoreEnvFile: true,
   ...configDefaultOptions,
 };
