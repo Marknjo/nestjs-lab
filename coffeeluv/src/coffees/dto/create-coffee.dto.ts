55c76360074bd4c08f4edbcd3ab838131a62eb92
Marknjo <marknjo7@gmail.com>
2022-10-11 11:48:22 +0300

    doc(coffeeluv): add api description to the create coffee dto

--- coffeeluv/src/coffees/dto/create-coffee.dto.ts
+++ coffeeluv/src/coffees/dto/create-coffee.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of a coffee' })
   @MaxLength(50)
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The coffee brand name' })
   @MaxLength(40)
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({
+    description: 'Different coffee flavors',
+    example: ['brown coffee', 'maven brown coffee'],
+  })
   @IsString({ each: true })
   readonly flavors: Array<string>;
 }
