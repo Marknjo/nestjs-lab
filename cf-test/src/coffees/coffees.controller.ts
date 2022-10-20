2abc5da207059859fc73adf249717f5460ef5f98
Marknjo <marknjo7@gmail.com>
2022-10-20 11:24:39 +0300

    ci(cf-test): add a health check route handler

--- cf-test/src/coffees/coffees.controller.ts
+++ cf-test/src/coffees/coffees.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get('/health')
+  healthCheck() {
+    return `
+      <div style="
+           margin: 30px auto; 
+           display: flex; 
+           flex-direction: 
+           column; 
+           align-items: center">
+        <h1 style="
+           line-height: 1; 
+           margin: 5px 0;
+           text-transform: capitalize;
+           ">Welcome to the coffees API</h1>
+        <p>API working as expected</p>
+      </div>
+    `;
+  }
+}
