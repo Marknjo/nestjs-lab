9f1ee8a0da80b9023a226a33c4a7249b21bffbfb
Marknjo <marknjo7@gmail.com>
2022-10-21 11:37:56 +0300

    feat(cf-test): add typeorm postgres db connection via docker

--- cf-test/src/app.module.ts
+++ cf-test/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'coffees',
+      password: 'cf-dev-secret',
+      database: 'postgres',
+      autoLoadEntities: true,
+      ...(env.NODE_ENV === 'production' ? {} : { synchronize: true }),
+    }),
+  ],
   controllers: [],
 })
 export class AppModule {}
