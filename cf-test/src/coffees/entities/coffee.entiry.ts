f12b4e20439002fe7bf463139df568644f0196f9
Marknjo <marknjo7@gmail.com>
2022-10-21 11:38:54 +0300

    feat(cf-test): add coffees entity repository

---
+++ cf-test/src/coffees/entities/coffee.entiry.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('coffees')
+export class Coffee {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  brand: string;
+
+  @Column('json', { nullable: true })
+  flavors: Array<string>;
+}
