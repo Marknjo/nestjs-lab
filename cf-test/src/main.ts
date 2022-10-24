753c022e5b7d0212cd9b93171e8722318eeeca3e
Marknjo <marknjo7@gmail.com>
2022-10-24 11:45:44 +0300

    feat(cf-test): add a logger on app bootup

--- cf-test/src/main.ts
+++ cf-test/src/main.ts
@@ -1,8 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = env.PORT || 3000;
+  await app.listen(port);
+
+  /// Logger
+  logger.log(
+    `Server running on http://localhost:${port}, view health: http://localhost:${port}/coffees/health`,
+  );
+  logger.log(`Server health check - http://localhost:${port}/coffees/health`);
 }
 bootstrap();
