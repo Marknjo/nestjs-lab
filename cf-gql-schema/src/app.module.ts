d2cf19aba734d68e63325e7d1805ed6e2b9b0387
Marknjo <marknjo7@gmail.com>
2022-11-09 12:10:33 +0300

    ci(cf-schema): connection to postgres using TypeOrm

--- cf-gql-schema/src/app.module.ts
+++ cf-gql-schema/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { envLoaderOptions } from './common/configs/env-loader-options';
 
 @Module({
-  imports: [ConfigModule.forRoot(envLoaderOptions)],
+  imports: [
+    ConfigModule.forRoot(envLoaderOptions),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        port: config.get('DB_PORT'),
+        host: config.get('DB_HOST'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        ...(config.get('NODE_ENV') !== 'production'
+          ? { synchronize: true }
+          : {}),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
