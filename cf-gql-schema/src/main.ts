6b15e0c6277e1b8eebeddc45dd43059324003b0f
Marknjo <marknjo7@gmail.com>
2022-11-09 11:25:10 +0300

    ci(cf-schema): add postgres db using docker compose

--- cf-gql-schema/src/main.ts
+++ cf-gql-schema/src/main.ts
@@ -1,11 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { randomBytes } from 'crypto';
 import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
+  // console.log(randomBytes(24).toString('base64'));
 
   const port = env.PORT || 3000;
   await app.listen(port);
