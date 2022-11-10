96eceacb75ef8ae31b1b7819312514551941f1cc
Marknjo <marknjo7@gmail.com>
2022-11-10 11:46:30 +0300

    feat(cf-schema): install & configure autogeneration of gql types

---
+++ cf-gql-schema/src/graphql.ts
@@ -0,0 +1,22 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export abstract class IQuery {
+    coffees: Coffee[];
+}
+
+export class Coffee {
+    id: string;
+    name: string;
+    brand: string;
+    flavors: string[];
+}
+
+type Nullable<T> = T | null;
